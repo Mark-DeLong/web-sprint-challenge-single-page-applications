d17bec66e89baa526e1876f8c965b8c38382e248
Mark <mark@Marks-MacBook-Air.local>
2020-12-18 11:58:03 -0500

    made scaffolding for App and Index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Link, Switch } from 'react-router-dom'
+
+import Pizza from './Pizza'
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <p>Get yo pizza</p>
+      <Link to="/">Home</Link>
+      <Link to="/Pizza">Click for pizza</Link>
+    <Switch>
+      <Route path="/">
+        <Form />
+      </Route>
+      <Route path="/Pizza/:itemId">
+        <Item items={toppings} />
+      </Route>
+      <Route path="/Pizza">
+        <ItemList items={toppings} />
+      </Route>
+    </Switch>
     </>
   );
 };
